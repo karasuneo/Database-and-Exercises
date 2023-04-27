19726ca06aef76b89088bb728b68ea1cc25ac713
Yusei Mizutani <yusei4520030401@gmail.com>
2023-04-27 22:38:37 +0900

    課題2

--- src/02/2-4.php
+++ src/02/2-4.php
@@ -1,7 +1,5 @@
 <?php 
-$member = array("name" => "○田○夫",
-                "tel"  => "03-0000-0000",
-                "address" => "東京都千代田区");
+$member = array("name" => "○田○夫", "tel"  => "03-0000-0000", "address" => "東京都千代田区");
 
 foreach($member as $key => $value) {
     print "$key: $value";
---
+++ src/02/2-5.php
@@ -0,0 +1,8 @@
+<?php
+function test($name) {
+    $str = $name . "さん、こんにちは!";
+    return $str;
+}
+
+print test("kkk");
+?>
