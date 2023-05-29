cfe02b3778cfffb5130e369deff731069515a1a1
Yusei Mizutani <yusei4520030401@gmail.com>
2023-05-29 13:56:41 +0900

    中間レポートのソースコード

--- README.md
+++ README.md
@@ -20,3 +20,22 @@ docker exec -it Database_And_Exercises_DB bash
 ```
 mysql -u root -p
 ```
+
+## texの使い方
+### Dockerイメージのpull
+```
+docker pull ghcr.io/being24/latex-docker:latest
+```
+
+### docker imageの起動
+```
+docker run --rm --name latex-template-ja -it -v ${PWD}:/workdir ghcr.io/being24/latex-docker
+```
+
+### pdfファイルの作成
+```
+uplatex main.tex
+```
+```
+dvipdfmx main
+```
--- src/06/8-3.php
+++ src/06/8-3.php
@@ -1,12 +1,18 @@
 <?php
-$db_user = "root";
-$db_pass = "admin";
+$db_user = "user";
+$db_pass = "f3u6mi";
 $db_host = "Database_And_Exercises_DB";
 $db_name = "mydb";
 $db_type = "mysql";
 $dsn = "$db_type:host=$db_host;dbname=$db_name;charset=utf8";
 
-$pdo = new PDO($dsn, $db_user, $db_pass);
+try {
+    $pdo = new PDO($dsn, $db_user, $db_pass);
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit(1);
+}
 
 $sql = "INSERT INTO member (last_name, first_name, age) VALUES ( :last_name, :first_name, :age)";
 $stmh = $pdo->prepare($sql);
